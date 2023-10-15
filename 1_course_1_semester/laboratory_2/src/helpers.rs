98e0953049aad8ef8af82e1cf3837de827a98137
Dmitriy-U <dmitriyushkalov@gmail.com>
2023-10-16 01:26:25 +0300

    Доделал выбор файлов

--- 1_course_1_semester/laboratory_2/src/helpers.rs
+++ 1_course_1_semester/laboratory_2/src/helpers.rs
@@ -1,4 +1,8 @@
-use std::io;
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
 use regex::Regex;
 
 fn validate_input_string_by_sequence_of_digits(string: &String) -> bool {
@@ -19,7 +23,7 @@ fn validate_input_string_by_boolean(string: &String) -> bool {
     re.is_match(match_string)
 }
 
-fn convert_input_to_vector(string_source: String) -> Vec<i32> {
+pub fn convert_input_to_vector(string_source: String) -> Vec<i32> {
     let mut vector: Vec<i32> = Vec::new();
     let string_split_sequence: Vec<&str> = string_source.split(" ").collect();
     for string_item in string_split_sequence {
@@ -44,7 +48,7 @@ fn get_user_input_string() -> String {
 
 pub enum SortingDirection {
     ASC,
-    DESC
+    DESC,
 }
 
 pub fn input_sorting_direction() -> SortingDirection {
@@ -55,30 +59,31 @@ pub fn input_sorting_direction() -> SortingDirection {
         user_input = String::from(user_input.trim());
 
         if validate_input_string_by_sorting_direction(&user_input) {
-            break if user_input == String::from("asc") {SortingDirection::ASC} else {SortingDirection::DESC}
+            break if user_input == String::from("asc") { SortingDirection::ASC } else { SortingDirection::DESC };
         } else {
             println!("You need to type \"asc\" or \"desc\"");
         }
     }
 }
 
-pub fn get_input_user_data() -> (i32, i32) {
+pub fn get_input_user_selection_conditions() -> (i32, i32) {
     let user_input = loop {
         let mut user_input = String::new();
-        println!("Enter students.txt count and debt separated by whitespace. Example: 5 1:");
-        io::stdin().read_line(&mut user_input).expect("An incorrect students.txt count and debt separated was entered. Sample: 5 1");
+        println!("Enter a number of students and number of exam backlogs separated by whitespace. Example: 5 1:");
+        io::stdin().read_line(&mut user_input).expect("An incorrect mockup.txt count and debt separated was entered. Sample: 5 1");
         user_input = String::from(user_input.trim());
         user_input.replace("  ", " ");
 
+        // TODO: Add check elements count "n" and "k"
         if validate_input_string_by_sequence_of_digits(&user_input) {
-            break user_input
+            break user_input;
         } else {
-            println!("You need to type students.txt count and debt");
+            println!("You need to type number of students and number of exam backlogs");
         }
     };
 
     let user_input = convert_input_to_vector(user_input);
-    (user_input[0], user_input[2])
+    (user_input[0], user_input[1])
 }
 
 fn get_user_sequence_of_integers(min_length: usize) -> Vec<i32> {
@@ -135,7 +140,7 @@ pub fn get_user_two_dimensional_sequence() -> Vec<Vec<i32>> {
 
 fn check_sequence_by_sorting_direction(
     sequence: &Vec<i32>,
-    sorting_direction: &SortingDirection
+    sorting_direction: &SortingDirection,
 ) -> bool {
     let mut temp: i32 = 0;
     let mut result = true;
@@ -162,7 +167,7 @@ fn check_sequence_by_sorting_direction(
 
 pub fn check_two_dimensional_sequence_by_sorting_direction(
     two_dimensional_sequence: Vec<Vec<i32>>,
-    sorting_direction: SortingDirection
+    sorting_direction: SortingDirection,
 ) -> Vec<usize> {
     let mut wrong_row_indexes: Vec<usize> = Vec::new();
 
@@ -173,4 +178,42 @@ pub fn check_two_dimensional_sequence_by_sorting_direction(
     }
 
     wrong_row_indexes
+}
+
+pub fn read_lines<P>(file_name: P) -> io::Result<io::Lines<io::BufReader<File>>>
+    where P: AsRef<Path>, {
+    let file = File::open(file_name)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub fn convert_string_to_user_with_grades(row: String) -> (String, Vec<i8>) {
+    let mut grades: Vec<i8> = Vec::new();
+    let mut file_row_split: Vec<&str> = row.split(" ").collect();
+    let student_id = file_row_split[0];
+    file_row_split.remove(0);
+    for string_item in file_row_split {
+        grades.push(string_item.to_string().parse().unwrap());
+    }
+    (student_id.to_string(), grades)
+}
+
+pub fn get_data_from_attributes() -> Option<HashMap<String, Vec<i8>>> {
+    match env::args().nth(1) {
+        Some(file_path) => {
+            match read_lines(file_path) {
+                Ok(file_lines) => {
+                    let mut students: HashMap<String, Vec<i8>> = HashMap::new();
+                    for file_line in file_lines {
+                        if let Ok(file_row) = file_line {
+                            let (student_id, grades) = convert_string_to_user_with_grades(file_row);
+                            students.insert(student_id, grades);
+                        }
+                    }
+                    Some(students)
+                }
+                _ => None
+            }
+        }
+        _ => None
+    }
 }
--- 1_course_1_semester/laboratory_2/src/main.rs
+++ 1_course_1_semester/laboratory_2/src/main.rs
@@ -1,32 +1,22 @@
 mod helpers;
 
-use std::env;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-fn read_lines<P>(file_name: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
-    let file = File::open(file_name)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
+use std::collections::HashMap;
+use crate::helpers::{get_data_from_attributes, get_input_user_selection_conditions};
 
 fn main() {
-    match env::args().nth(1) {
-        Some(file_path) => {
-            if let Ok(lines) = read_lines(file_path) {
-                // Получает итератор, который возвращает Option
-                for line in lines {
-                    if let Ok(ip) = line {
-                        println!("{}", ip);
-                    }
-                }
-            }
+    let examBacklogValues: [i8;2] = [0, 2];
+    let mut student_list: HashMap<String, Vec<i8>> = HashMap::new();
+
+    match get_data_from_attributes() {
+        Some(list) => {
+            student_list = list
         }
-        _ => {
-            println!("None");
-        }
-    };
-    // let (n, k) = get_input_user_data();
+        None => {}
+    }
+
+    let (n, k) = get_input_user_selection_conditions();
+
+    // TODO: Complete loop typing of student list
+
+    // TODO: Complete student list sorting
 }
