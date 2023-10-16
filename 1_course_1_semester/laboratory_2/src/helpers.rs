5653edc6c95f525e58f875e8afbce31e2acac405
Dmitriy-U <dmitriyushkalov@gmail.com>
2023-10-17 00:25:51 +0300

    Added a backlog filter

--- 1_course_1_semester/laboratory_2/src/helpers.rs
+++ 1_course_1_semester/laboratory_2/src/helpers.rs
@@ -11,19 +11,13 @@ fn validate_input_string_by_sequence_of_digits(string: &String) -> bool {
     re.is_match(match_string)
 }
 
-fn validate_input_string_by_sorting_direction(string: &String) -> bool {
-    let re = Regex::new(r"^(asc|desc)$").unwrap();
+fn validate_input_string_by_student_id_with_grades(string: &String) -> bool {
+    let re = Regex::new(r"^[\w-]+ [\d ]+$").unwrap();
     let match_string = &string[..];
     re.is_match(match_string)
 }
 
-fn validate_input_string_by_boolean(string: &String) -> bool {
-    let re = Regex::new(r"^(y|n)$").unwrap();
-    let match_string = &string[..];
-    re.is_match(match_string)
-}
-
-pub fn convert_input_to_vector(string_source: String) -> Vec<i32> {
+fn convert_input_to_vector(string_source: String) -> Vec<i32> {
     let mut vector: Vec<i32> = Vec::new();
     let string_split_sequence: Vec<&str> = string_source.split(" ").collect();
     for string_item in string_split_sequence {
@@ -32,161 +26,80 @@ pub fn convert_input_to_vector(string_source: String) -> Vec<i32> {
     vector
 }
 
-fn get_user_input_string() -> String {
-    let mut user_input = String::new();
-    println!("Enter a sequence of integers separated by spaces:");
-    io::stdin().read_line(&mut user_input).expect("An incorrect sequence of integers was entered. Sample: 12 345 6 7890");
-    user_input = String::from(user_input.trim());
-    println!("Typed: {}", user_input);
-
-    if !validate_input_string_by_sequence_of_digits(&user_input) {
-        user_input = get_user_input_string();
-    }
-
-    user_input
-}
-
-pub enum SortingDirection {
-    ASC,
-    DESC,
-}
-
-pub fn input_sorting_direction() -> SortingDirection {
+fn get_input_user_student() -> (String, Vec<i8>) {
     loop {
+        println!("Enter a row of student id with grades separated by whitespace. Example: 1a8a9ea3-24e2-4975-aa10-b73ae1a822e7 5 3 4 4 4 4 4 0 2 4:");
         let mut user_input = String::new();
-        println!("Enter a sorting_direction. \"asc\" or \"desc\":");
-        io::stdin().read_line(&mut user_input).expect("An incorrect sequence of integers was entered. Sample: \"asc\" or \"desc\"");
+        io::stdin().read_line(&mut user_input).expect("An incorrect row was entered. Sample: 1a8a9ea3-24e2-4975-aa10-b73ae1a822e7 5 3 4 4 4 4 4 0 2 4");
         user_input = String::from(user_input.trim());
+        user_input = user_input.replace("  ", " ");
 
-        if validate_input_string_by_sorting_direction(&user_input) {
-            break if user_input == String::from("asc") { SortingDirection::ASC } else { SortingDirection::DESC };
+        if validate_input_string_by_student_id_with_grades(&user_input) {
+            break convert_string_to_user_with_grades(user_input)
         } else {
-            println!("You need to type \"asc\" or \"desc\"");
+            println!("You need to type number of students and number of backlogs");
         }
     }
 }
 
-pub fn get_input_user_selection_conditions() -> (i32, i32) {
+pub fn get_input_user_student_list(student_count: usize) -> HashMap<String, Vec<i8>> {
+    let mut student_list: HashMap<String, Vec<i8>> = HashMap::new();
+
+    for index in 0..student_count {
+        println!("Student {}", index + 1);
+
+        let (user_id, grades) = get_input_user_student();
+        student_list.insert(user_id, grades);
+    };
+
+    student_list
+}
+
+pub fn get_input_user_selection_conditions() -> (usize, usize) {
     let user_input = loop {
         let mut user_input = String::new();
-        println!("Enter a number of students and number of exam backlogs separated by whitespace. Example: 5 1:");
-        io::stdin().read_line(&mut user_input).expect("An incorrect mockup.txt count and debt separated was entered. Sample: 5 1");
+        println!("Enter a number of students and number of backlogs separated by whitespace. Example: 5 1:");
+        io::stdin().read_line(&mut user_input).expect("An incorrect typed data. Sample: 5 1");
         user_input = String::from(user_input.trim());
-        user_input.replace("  ", " ");
+        user_input = user_input.replace("  ", " ");
 
-        // TODO: Add check elements count "n" and "k"
         if validate_input_string_by_sequence_of_digits(&user_input) {
             break user_input;
         } else {
-            println!("You need to type number of students and number of exam backlogs");
+            println!("You need to type number of students and number of backlogs");
         }
     };
 
     let user_input = convert_input_to_vector(user_input);
-    (user_input[0], user_input[1])
+    (user_input[0] as usize, user_input[1] as usize)
 }
 
-fn get_user_sequence_of_integers(min_length: usize) -> Vec<i32> {
-    let user_input_string = get_user_input_string();
-    let input_vector = convert_input_to_vector(user_input_string);
-
-    if input_vector.len() < min_length {
-        println!("Please, type sequence with {min_length} integers minimum1");
-        get_user_sequence_of_integers(min_length)
-    } else {
-        input_vector
-    }
-}
-
-fn input_need_to_continue() -> bool {
-    loop {
-        println!("Need to continue. Type \"y\" for yes or \"n\" for no:");
+pub fn get_input_user_selection_condition() -> usize {
+    let user_input = loop {
         let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input).expect("An incorrect was entered. Sample: \"y\" or \"n\"");
+        println!("Enter a number of backlogs separated by whitespace. Example: 5:");
+        io::stdin().read_line(&mut user_input).expect("An incorrect typed data. Sample: 5");
         user_input = String::from(user_input.trim());
+        user_input = user_input.replace("  ", " ");
 
-        if validate_input_string_by_boolean(&user_input) {
-            break user_input == String::from("y");
+        if validate_input_string_by_sequence_of_digits(&user_input) {
+            break user_input;
         } else {
-            println!("You need to type \"y\" or \"n\"");
+            println!("You need to type number of backlogs");
         }
-    }
+    };
+
+    let user_input = convert_input_to_vector(user_input);
+    user_input[0] as usize
 }
 
-pub fn get_user_two_dimensional_sequence() -> Vec<Vec<i32>> {
-    let mut user_two_dimensional_sequence: Vec<Vec<i32>> = Vec::new();
-
-    let mut row: usize = 1;
-    loop {
-        println!("{row} row:");
-        let user_sequence_of_digits = get_user_sequence_of_integers(1);
-        user_two_dimensional_sequence.push(user_sequence_of_digits);
-
-        if !input_need_to_continue() { break; }
-
-        row += 1;
-    }
-
-    println!("Typed two-dimensional sequence:");
-    for row_index in 0..user_two_dimensional_sequence.len() {
-        for item_index in 0..user_two_dimensional_sequence[row_index].len() {
-            print!("{} ", user_two_dimensional_sequence[row_index][item_index]);
-        }
-        println!("");
-    }
-
-    user_two_dimensional_sequence
-}
-
-fn check_sequence_by_sorting_direction(
-    sequence: &Vec<i32>,
-    sorting_direction: &SortingDirection,
-) -> bool {
-    let mut temp: i32 = 0;
-    let mut result = true;
-
-    for item_index in 0..sequence.len() {
-        if item_index == 0 {
-            temp = sequence[item_index];
-            continue;
-        }
-
-        let condition_result = match sorting_direction {
-            SortingDirection::ASC => sequence[item_index] > temp,
-            SortingDirection::DESC => sequence[item_index] < temp
-        };
-
-        if !condition_result {
-            result = false;
-            break;
-        }
-    }
-
-    result
-}
-
-pub fn check_two_dimensional_sequence_by_sorting_direction(
-    two_dimensional_sequence: Vec<Vec<i32>>,
-    sorting_direction: SortingDirection,
-) -> Vec<usize> {
-    let mut wrong_row_indexes: Vec<usize> = Vec::new();
-
-    for row_index in 0..two_dimensional_sequence.len() {
-        if !check_sequence_by_sorting_direction(&two_dimensional_sequence[row_index], &sorting_direction) {
-            wrong_row_indexes.push(row_index);
-        }
-    }
-
-    wrong_row_indexes
-}
-
-pub fn read_lines<P>(file_name: P) -> io::Result<io::Lines<io::BufReader<File>>>
+fn read_lines<P>(file_name: P) -> io::Result<io::Lines<io::BufReader<File>>>
     where P: AsRef<Path>, {
     let file = File::open(file_name)?;
     Ok(io::BufReader::new(file).lines())
 }
 
-pub fn convert_string_to_user_with_grades(row: String) -> (String, Vec<i8>) {
+fn convert_string_to_user_with_grades(row: String) -> (String, Vec<i8>) {
     let mut grades: Vec<i8> = Vec::new();
     let mut file_row_split: Vec<&str> = row.split(" ").collect();
     let student_id = file_row_split[0];
@@ -216,4 +129,17 @@ pub fn get_data_from_attributes() -> Option<HashMap<String, Vec<i8>>> {
         }
         _ => None
     }
+}
+
+pub fn get_backlog_student_list(student_list: HashMap<String, Vec<i8>>, backlog_values: [i8; 2], min_block_numbers: usize) -> HashMap<String, Vec<i8>> {
+    let mut backlog_student_list: HashMap<String, Vec<i8>> = HashMap::new();
+
+    for (student_id, grades) in student_list {
+        let backlog_grades = grades.into_iter().filter(|&grade| backlog_values.contains(&grade)).collect::<Vec<_>>();
+        if backlog_grades.len() >= min_block_numbers {
+            backlog_student_list.insert(student_id, backlog_grades);
+        }
+    }
+
+    backlog_student_list
 }
--- 1_course_1_semester/laboratory_2/src/main.rs
+++ 1_course_1_semester/laboratory_2/src/main.rs
@@ -1,10 +1,17 @@
 mod helpers;
 
 use std::collections::HashMap;
-use crate::helpers::{get_data_from_attributes, get_input_user_selection_conditions};
+use crate::helpers::{
+    get_backlog_student_list,
+    get_data_from_attributes,
+    get_input_user_selection_condition,
+    get_input_user_selection_conditions,
+    get_input_user_student_list,
+};
+
+const BACKLOG_VALUES: [i8; 2] = [0, 2];
 
 fn main() {
-    let examBacklogValues: [i8;2] = [0, 2];
     let mut student_list: HashMap<String, Vec<i8>> = HashMap::new();
 
     match get_data_from_attributes() {
@@ -14,9 +21,23 @@ fn main() {
         None => {}
     }
 
-    let (n, k) = get_input_user_selection_conditions();
+    let min_block_numbers = if student_list.len() == 0 {
+        let (n, k) = get_input_user_selection_conditions();
+        student_list = get_input_user_student_list(n);
+        k
+    } else {
+        get_input_user_selection_condition()
+    };
 
-    // TODO: Complete loop typing of student list
+    let backlog_student_list = get_backlog_student_list(student_list, BACKLOG_VALUES, min_block_numbers);
 
-    // TODO: Complete student list sorting
+    match backlog_student_list.len() {
+        0 => { println!("There are no backlog students"); }
+        _ => {
+            println!("Result:");
+            for (student_id, _) in backlog_student_list {
+                println!("{student_id}");
+            }
+        }
+    }
 }
